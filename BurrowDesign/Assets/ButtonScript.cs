07409941e0729085ca2fa3a14466525ca7431330
wrv237 <wrv237@alumni.ku.dk>
2020-10-23 09:48:19 +0200

    u

--- BurrowDesign/Assets/ButtonScript.cs
+++ BurrowDesign/Assets/ButtonScript.cs
@@ -6,16 +6,40 @@ public class ButtonScript : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject door;
+    public Vector3 startPos;
+    public GameObject button;
 
     // Update is called once per frame
+
+    void Start()
+    {
+        startPos  = button.transform.position;
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            Destroy(door);
+            Debug.Log(door.active);
+            if (door.active)
+            {
+                door.SetActive(false);
+            }
+            else
+            {
+                door.SetActive(true);
+            }
             Vector3 newPos = transform.position;
-            newPos.y = -0.4f;
-            transform.position = newPos;
+            newPos.y = -0.2f;
+            button.transform.position = newPos;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            button.transform.position = startPos;
         }
     }
 }
